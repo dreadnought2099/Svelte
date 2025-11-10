d13d619daf238db8cee8cac71f6f119e97b0f2e0
Raymart Magallanes <magallanesraymart@gmail.com>
2025-11-11 07:58:38 +0800

    changes in loading progress gradient color

--- resources/js/Components/Navbar.svelte
+++ resources/js/Components/Navbar.svelte
@@ -13,7 +13,7 @@
     }
 </script>
 
-<nav class="bg-gray-800 shadow-lg">
+<nav class="bg-primary shadow-lg">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
