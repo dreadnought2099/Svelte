4edf2f4f1e412c1964f9b7249e995aef28d7e4be
Raymart Magallanes <magallanesraymart@gmail.com>
2025-11-10 23:53:53 +0800

    customized the progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ import "./bootstrap";
 import { createInertiaApp } from "@inertiajs/svelte";
 import { mount } from "svelte";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import NProgress from "nprogress";
+import { router } from "@inertiajs/svelte";
+
+// disable spinner completely
+NProgress.configure({ showSpinner: false });
 
 createInertiaApp({
     resolve: (name) =>
@@ -12,4 +17,10 @@ createInertiaApp({
     setup({ el, App, props }) {
         mount(App, { target: el, props });
     },
+    progress: false,
 });
+
+router.on("start", () => NProgress.start());
+router.on("finish", () => NProgress.done());
+router.on("error", () => NProgress.done());
+router.on("cancel", () => NProgress.done());
