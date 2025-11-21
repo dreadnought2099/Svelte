1ff1fb910fb3276210e74720e16d5d06208ee310
Raymart Magallanes <magallanesraymart@gmail.com>
2025-11-21 15:03:54 +0800

    minor changes

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -6,7 +6,7 @@
     <div class="bg-white rounded-lg shadow-md p-8">
         <h1 class="text-4xl font-bold text-gray-900 mb-4">Welcome Home</h1>
         <p class="text-gray-600">
-            This is the home page with no page reload navigation!
+            This is the home page!
         </p>
     </div>
 </AppLayout>
